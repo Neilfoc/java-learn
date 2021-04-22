8cdd053097925b7590d008dd45c1f26cd1d53efa
11105157 <1319025936@qq.com>
2021-04-22 17:17:00 +0800

    http请求

---
+++ javase/src/http/RESTTemplate.java
@@ -0,0 +1,9 @@
+package http;
+
+/**
+ * @author 11105157
+ * @Description
+ * @Date 2021/4/21
+ */
+public class RESTTemplate {
+}
---
+++ javase/src/http/Test.java
@@ -0,0 +1,6 @@
+package http;/**
+ * @author 11105157
+ * @Description
+ * @Date 2021/4/21
+ */public class Test {
+}
