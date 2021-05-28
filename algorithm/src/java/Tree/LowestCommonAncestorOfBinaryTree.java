c447bae9dffb1ca6b8ae08fad77bf3ca5c5bb276
11105157 <1319025936@qq.com>
2021-05-28 17:23:40 +0800

    mad

--- algorithm/src/java/Tree/LowestCommonAncestorOfBinaryTree.java
+++ algorithm/src/java/Tree/LowestCommonAncestorOfBinaryTree.java
@@ -7,7 +7,19 @@ package Tree;
  */
 public class LowestCommonAncestorOfBinaryTree {
 
+    // 使用递归
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        return left != null ? left : right;
     }
 }
