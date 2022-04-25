7987fe3741e1b10d229fce795eef78e7ea679482
neilfoc <1319025936@qq.com>
2022-04-26 00:56:24 +0800

    IO

---
+++ javase/src/main/java/socket/zhouzhilei/SocketClient.java
@@ -0,0 +1,33 @@
+package socket.zhouzhilei;
+
+import java.io.*;
+import java.net.Socket;
+
+public class SocketClient {
+
+    public static void main(String[] args) {
+
+        try {
+            Socket client = new Socket("192.168.150.11",9090);
+
+            client.setSendBufferSize(20);
+            client.setTcpNoDelay(true);
+            OutputStream out = client.getOutputStream();
+
+            InputStream in = System.in;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            while(true){
+                String line = reader.readLine();
+                if(line != null ){
+                    byte[] bb = line.getBytes();
+                    for (byte b : bb) {
+                        out.write(b);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ javase/src/main/java/socket/zhouzhilei/SocketNIO.java
@@ -0,0 +1,63 @@
+package socket.zhouzhilei;
+
+import java.net.InetSocketAddress;
+import java.net.StandardSocketOptions;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.LinkedList;
+
+public class SocketNIO {
+
+    public static void main(String[] args) throws Exception {
+
+        LinkedList<SocketChannel> clients = new LinkedList<>();
+
+        ServerSocketChannel ss = ServerSocketChannel.open();
+        ss.bind(new InetSocketAddress(9090));
+        ss.configureBlocking(false); //重点  OS  NONBLOCKING!!!
+
+        ss.setOption(StandardSocketOptions.TCP_NODELAY, false);
+//        StandardSocketOptions.TCP_NODELAY
+//        StandardSocketOptions.SO_KEEPALIVE
+//        StandardSocketOptions.SO_LINGER
+//        StandardSocketOptions.SO_RCVBUF
+//        StandardSocketOptions.SO_SNDBUF
+//        StandardSocketOptions.SO_REUSEADDR
+
+
+
+
+        while (true) {
+            Thread.sleep(1000);
+            SocketChannel client = ss.accept(); //不会阻塞？  -1NULL
+
+            if (client == null) {
+                System.out.println("null.....");
+            } else {
+                client.configureBlocking(false);
+                int port = client.socket().getPort();
+                System.out.println("client...port: " + port);
+                clients.add(client);
+            }
+
+            ByteBuffer buffer = ByteBuffer.allocateDirect(4096);  //可以在堆里   堆外
+
+            for (SocketChannel c : clients) {   //串行化！！！！  多线程！！
+                int num = c.read(buffer);  // >0  -1  0   //不会阻塞
+                if (num > 0) {
+                    buffer.flip();
+                    byte[] aaa = new byte[buffer.limit()];
+                    buffer.get(aaa);
+
+                    String b = new String(aaa);
+                    System.out.println(c.socket().getPort() + " : " + b);
+                    buffer.clear();
+                }
+
+
+            }
+        }
+    }
+
+}
