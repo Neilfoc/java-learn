25e9c30a375cdefff251a78e1bedff1c133565d0
neilfoc <1319025936@qq.com>
2022-05-25 00:22:46 +0800

    onjava8并发初学

---
+++ javase/src/main/java/onJava8/concurrent24/ParallelPrime.java
@@ -0,0 +1,38 @@
+package onJava8.concurrent24;
+
+import java.util.*;
+import java.util.stream.*;
+
+import static java.util.stream.LongStream.*;
+
+import java.io.*;
+import java.nio.file.*;
+
+import onJava8.concurrent24.onjava.Timer;
+
+public class ParallelPrime {
+    static final int COUNT = 100_000;
+
+    public static boolean isPrime(long n) {
+        return rangeClosed(2, (long) Math.sqrt(n))
+                .noneMatch(i -> n % i == 0);
+    }
+
+    public static void main(String[] args)
+            throws IOException {
+        Timer timer = new Timer();
+        List<String> primes =
+                iterate(2, i -> i + 1)
+                        .parallel()                       // [1]
+                        .filter(ParallelPrime::isPrime)
+                        .limit(COUNT)
+                        .mapToObj(Long::toString)
+                        .collect(Collectors.toList());
+        System.out.println(timer.duration());
+        Files.write(Paths.get("primes.txt"), primes,
+                StandardOpenOption.CREATE);
+    }
+}
+/* Output:
+1635
+*/
