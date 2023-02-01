b73b57783915ca31f8129650bfc92b46d78318d9
neilfoc <1319025936@qq.com>
2023-02-01 22:58:21 +0800

    只加入slf4j-api

---
+++ javase/src/main/java/App.java
@@ -0,0 +1,12 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+    // 源码分析
+    final static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
+}
